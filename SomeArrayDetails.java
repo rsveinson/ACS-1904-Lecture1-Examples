633a24c5ab39c1376de8a0ec022a7a04d34a1e42
rsveinson <rsveinson@mts.net>
2023-01-09 08:11:51 -0600

    added some partial array code added a print to see what info is stored in the array variable

---
+++ SomeArrayDetails.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+import java.util.Random;
+
+/** 
+ * ACS-1904 Assignment X Question Y
+ * @author 
+*/
+
+public class SomeArrayDetails{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int[] list = new int[10];
+        Random r = new Random();
+        int randomNumber;
+        int count;          // index and counter for the array
+        
+        count = 0;          // explicitly set the value of the index
+        randomNumber = r.nextInt(11);
+        
+        while(randomNumber != 0){
+            // add the random number to the array at index i
+            // increment i so the next number will be added
+            // at index i + 1
+            list[count++] = randomNumber;
+            randomNumber = r.nextInt(11);
+        }// end while
+        
+        // print the array
+        for(int n : list){
+            System.out.println(n);
+        }// end for each
+        
+        // print only the used part of the array
+        for(int i = 0; i < count; i++){
+            System.out.println(list[i]);
+        }// end for
+        
+        System.out.println(list);
+        
+        
+
+
+        System.out.println("end of program");
+    }
+}
