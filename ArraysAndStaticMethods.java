ff198b2eede6195550cda391864d7272e11c2e20
rsveinson <rsveinson@mts.net>
2023-01-10 11:50:35 -0600

    added some examples of using stati methods with arrays

---
+++ ArraysAndStaticMethods.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+/** 
+ * ACS-1904 Assignment X Question Y
+ * @Sveinson
+ * 
+ * using static methods with arrays
+*/
+
+public class ArraysAndStaticMethods{
+    public static void main(String[] args) {
+        final int MAX = 10;
+        
+        int[] list = new int[MAX];
+        int count;                  // counter and index for the array
+        int inputNumber;            // numbers read from the keyboard
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        count = 0;          // explicily set the index/counter to 0
+        
+        // load the array with some number from the keyboard
+        System.out.println("enter up to 10 numbers, -1 to quit");
+        inputNumber = scanner.nextInt();
+        
+        while(inputNumber != -1){
+            //System.out.println(inputNumber);
+            
+            // add input to the list
+            list[count++] = inputNumber;
+            
+            // update control
+            inputNumber = scanner.nextInt();
+        }// end load array loop
+
+        // print the list 
+        System.out.println(list);
+        printList(list, count);
+        
+        // change an element using a static method
+        changeTheList(list);
+        
+        // print the list witht he change
+        printList(list, count);
+        
+        System.out.println("end of program");
+    }// end main
+    
+    // *** static methods ***
+    public static void printList(int[] l, int c){
+        for(int i = 0; i < c; i++){
+            System.out.println(l[i]);
+        }//end for
+    }// end print list
+    
+    public static void changeTheList(int[] l){
+        System.out.println(l);
+        l[0] = 7896456;
+    }// end change the list
+}
